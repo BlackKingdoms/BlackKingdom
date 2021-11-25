6b9fc0253597b18849d0c94a4de81d2e2ef41871
Black Kingdom <94972950+BlackKingdoms@users.noreply.github.com>
2021-11-25 19:59:01 +0530

    👹👹 BLACKKINGDOM WHATSAPP BOT👹👹
    👹👹 BLACKKINGDOM WHATSAPP BOT👹👹

---
+++ helpers.js
@@ -0,0 +1,28 @@
+/* Copyright (C) 2020 BlackKingdoms.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Maraya - BlackKingdoms
+*/
+
+function successfullMessage(msg) {
+    return "✅ *BlackKingdoms*:  ```" + msg + "```"
+}
+function errorMessage(msg) {
+    return "🛑 *BlackKingdoms*:  ```" + msg + "```"
+}
+function infoMessage(msg) {
+    return "⏺️ *BlackKingdoms*:  ```" + msg + "```"
+}
+
+
+module.exports = {
+    successfullMessage,
+    errorMessage,
+    infoMessage
+}
+
+
+/*BlackKingdoms whatsapp user bot public steble virsion 0.16.5 runing*/ /*(@adiwajshing/baileys)*/ /*whatsasena based*/
+/*powerd by BlackKingdoms creations/K.G LEGION CREW/AmdA/toxic whoman/kaviya tech/bad boys*/
