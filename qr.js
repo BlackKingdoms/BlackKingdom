f449e0d089bfadbf4dbbf38d567dc756f1b2e371
Black Kingdom <94972950+BlackKingdoms@users.noreply.github.com>
2021-11-26 15:10:27 +0530

    Create qr.js

---
+++ qr.js
@@ -0,0 +1,69 @@
+/* Copyright (C) 2020 Blackkingdom .
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Blackkingdom  - Blackkingdom 
+*/
+
+const chalk = require('chalk');
+const { WAConnection, MessageType } = require('@adiwajshing/baileys');
+const fs = require('fs');
+
+async function whatsAsena() {
+	const conn = new WAConnection();
+	conn.logger.level = 'warn';
+	conn.version = [3, 3234, 9]
+
+	conn.on('connecting', async () => {
+		console.log(`${chalk.green.bold('BLACK')}${chalk.blue.bold('KINGDOM')}
+${chalk.white.italic('BLACKKINGDOM  Strings')}
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
+	});
+
+	conn.on('open', async () => {
+		console.log(
+			chalk.green.bold('BLACKKINGDOM QR Code: '),
+			'BLACKKINGDOM ;;;' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+					'base64'
+				)
+		);
+		await conn.sendMessage(
+			conn.user.jid,
+			'BLACKKINGDOM;;;' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+					'base64'
+				),
+			MessageType.text
+		);
+		if (conn.user.jid.startsWith('94')) {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ Meka yavanna epa katawath ' + conn.user.name + '* ⚠️',
+				MessageType.text
+			);
+		} else {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ Please Do Not Share This Code With Anyone ' +
+					conn.user.name +
+					'* ⚠️',
+				MessageType.text
+			);
+		}
+		console.log(
+			chalk.green.bold(
+				"Meka copy karanna bari nam bn whatsapp eke athi bn code eka awith!\n"
+			),
+			chalk.green.bold(
+				'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!'
+			)
+		);
+		process.exit(0);
+	});
+
+	await conn.connect();
+}
+
+whatsAsena(); 
