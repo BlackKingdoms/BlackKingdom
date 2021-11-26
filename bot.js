8508606f594bac7dd8905c790be5da77a78ad4ab
Black Kingdom <94972950+BlackKingdoms@users.noreply.github.com>
2021-11-26 19:11:34 +0530

    👹👹 BLACKKINGDOM WHATSAPP BOT👹👹
    👹👹 BLACKKINGDOM WHATSAPP BOT👹👹

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Maraya.
+/* Copyright (C) 2020 BlackKingdoms.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Maraya - yasasdileepa
+BlackKingdom - BlackKingdoms
 */
 
 const fs = require("fs");
