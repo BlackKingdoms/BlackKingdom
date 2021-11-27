0c52f04d15808517e1306eb86f5dc33a871cae0b
Black Kingdom <94972950+BlackKingdoms@users.noreply.github.com>
2021-11-27 06:16:46 +0530

    👹👹 BLACKKINGDOM WHATSAPP BOT👹👹
    👹👹 BLACKKINGDOM WHATSAPP BOT👹👹

--- language/EN.js
+++ language/EN.js
@@ -2,7 +2,7 @@
     "__ASENAJSON__": true,
     "LANGUAGE": "English",
     "LANGCODE": "en",
-    "AUTHOR": "AsenaDev",
+    "AUTHOR": BlackKingdomDev",
     "STRINGS": {
         "_asena": {
             "COMMAND": "විධානය",
