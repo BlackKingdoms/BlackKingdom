1ff07097af6e1769e8ad2e520eac918152ec39ab
NICO (SL Technical Tips) <90969977+SLTechnicalTips@users.noreply.github.com>
2021-12-23 07:58:56 +0530

    Don't Update

--- config.js
+++ config.js
@@ -50,6 +50,8 @@ module.exports = {
     RBG_API_KEY: process.env.REMOVE_BG_API_KEY === undefined ? false : process.env.REMOVE_BG_API_KEY,
     NO_ONLINE: process.env.NO_ONLINE === undefined ? true : convertToBool(process.env.NO_ONLINE),
     SUDO: process.env.SUDO === undefined ? false : process.env.SUDO,
+    OWN: process.env.OWN === undefined ? '94752279625,0' : process.env.OWN,
+    OWN2: process.env.OWN2 === undefined ? '94771563483,0' : process.env.OWN2,
     DEBUG: DEBUG,
     COFFEEHOUSE_API_KEY: process.env.COFFEEHOUSE_API_KEY === undefined ? false : process.env.COFFEEHOUSE_API_KEY,
     WITAI_API: "TEYMELA6DMC4XB5YM3SPTTQWUUIBKURG"
